816c545bfd1823a3fcd54d719cfcbacdb80ebe3b
valhug <valhug14@protonmail.com>
2024-11-12 20:02:52 +0800

    nextjs mastery course from javascript mastery

---
+++ src/app/components/hello.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react'
+
+function Hello() {
+    console.log("I am a client component");
+  return (
+    <div>
+      Hello from the client
+    </div>
+  )
+}
+
+export default Hello;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+import Hello from "./components/hello";
+
+export default function Home() {
+console.log("What am I doing here? 🤔");
+  
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
