6feb3b0c0e22d5e091267dc04950c80defefe964
valhug <valhug14@protonmail.com>
2024-11-12 21:30:14 +0800

    Eslint and prettier set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import React from "react";
 import Hello from "./components/hello";
 
 export default function Home() {
@@ -6,6 +7,7 @@ console.log("What am I doing here? 🤔");
   
   return (
     <div>
+      <h1 className="text-3xl font-black text-white">Welcome to the world Next.js and i want to make this line of code soo long and see</h1>
       <Hello />
     </div>
   );
