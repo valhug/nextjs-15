577581e246b1ad09952ab1eaa85fef705d940192
valhug <valhug14@protonmail.com>
2024-11-14 13:25:58 +0800

    fix tailwind config to use global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,8 @@ const config: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
